3fb229ca2a22802bc77e016ced6c309ae530421e
yangyang910 <YANGXIN910@jk.cn>
2019-12-07 17:07:02 +0800

    Summary:<leetcode merge sorted arraies>

---
+++ LeetCode/MergeSortedArray.java
@@ -0,0 +1,38 @@
+package algorithm;
+
+import java.util.Arrays;
+
+/**
+ * Time: 2019-12-07
+ * Author:
+ * Mail:
+ * Description:
+ */
+public class MergeSortedArray {
+
+    public void merge(int[] nums1, int m, int[] nums2, int n) {
+        System.arraycopy(nums2, 0, nums1, m, n);
+        Arrays.sort(nums1);
+    }
+
+    public void merge_1(int[] nums1, int m, int[] nums2, int n) {
+        int [] nums1_copy = new int[m];
+        //拷贝数组
+        System.arraycopy(nums1, 0, nums1_copy, 0, m);
+
+        int p1 = 0;
+        int p2 = 0;
+        int p = 0;
+        while ((p1 < m) && (p2 < n)) {
+            nums1[p++] = (nums1_copy[p1] < nums2[p2]) ? nums1_copy[p1++] : nums2[p2++];
+        }
+
+        if (p1 < m) {
+            System.arraycopy(nums1_copy, p1, nums1, p1 + p2, m + n - p1 - p2);
+        }
+
+        if (p2 < n) {
+            System.arraycopy(nums2, p2, nums1, p1 + p2, m + n - p1 - p2);
+        }
+    }
+}
