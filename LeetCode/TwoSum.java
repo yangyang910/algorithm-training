a4e5d2a49d8da0cba7d9d02b4a56df62480a9e99
yangyang910 <YANGXIN910@jk.cn>
2019-12-09 09:41:04 +0800

    Summary:<leetcode two sum>

---
+++ LeetCode/TwoSum.java
@@ -0,0 +1,49 @@
+package algorithm;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Time: 2019-12-09
+ * Author:
+ * Mail:
+ * Description:
+ */
+public class TwoSum {
+    public int[] twoSum(int[] nums, int target) {
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = i + 1; j < nums.length; j++) {
+                if (nums[j] == target - nums[i]) {
+                    return new int[] { i, j };
+                }
+            }
+        }
+        return null;
+    }
+
+    public int[] twoSum_1(int[] nums, int target) {
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            map.put(nums[i], i);
+        }
+        for (int i = 0; i < nums.length; i++) {
+            int complement = target - nums[i];
+            if (map.containsKey(complement) && map.get(complement) != i) {
+                return new int[] { i, map.get(complement) };
+            }
+        }
+        return null;
+    }
+
+    public int[] twoSum_2(int[] nums, int target) {
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            int complement = target - nums[i];
+            if (map.containsKey(complement)) {
+                return new int[] { map.get(complement), i };
+            }
+            map.put(nums[i], i);
+        }
+        return null;
+    }
+}
